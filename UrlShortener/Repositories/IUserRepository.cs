1e93b8ed51b41900f547af1892f5c10e26a22b9a
Orangekun <amazingritro66@gmail.com>
2024-08-05 00:46:21 +0300

    added UserRepository

---
+++ UrlShortener/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+using UrlShortener.Models;
+
+namespace UrlShortener.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserByUserNameAsync(string userName);
+        Task<User> GetUserByIdAsync(int id);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+
+    }
+}
