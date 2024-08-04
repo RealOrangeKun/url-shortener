937325200244dd724b925ab6a7caf0507b330ec7
Orangekun <amazingritro66@gmail.com>
2024-08-05 00:44:33 +0300

    added Users model and overrided the onmodelCreation

--- UrlShortener/Data/UrlShortenerDbContext.cs
+++ UrlShortener/Data/UrlShortenerDbContext.cs
@@ -6,5 +6,16 @@ namespace UrlShortener.Data
     public class UrlShortenerDbContext(DbContextOptions<UrlShortenerDbContext> options) : DbContext(options)
     {
         public DbSet<Url> Urls { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Url>().HasKey(u => new { u.Id, u.UserId });
+            modelBuilder.Entity<User>().HasKey(u => u.Id);
+            modelBuilder.Entity<Url>().HasOne<User>().WithMany().HasForeignKey(u => u.UserId);
+            modelBuilder.Entity<User>().Property(u => u.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Url>().Property(u => u.Id).ValueGeneratedOnAdd();
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
