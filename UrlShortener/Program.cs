f0ffeeaf5c51930cc7ade225ca185563997a892b
Orangekun <amazingritro66@gmail.com>
2024-08-05 00:45:14 +0300

    added new utility to shorten the urls and added the userService and Repository as scoped

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -2,12 +2,16 @@ using Microsoft.EntityFrameworkCore;
 using UrlShortener.Data;
 using UrlShortener.Repositories;
 using UrlShortener.Services;
+using UrlShortener.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<UrlShortenerDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IUrlService, UrlService>();
 builder.Services.AddScoped<IUrlRepository, UrlRepository>();
+builder.Services.AddScoped<IUrlShortener, UrlShortenerUtility>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
