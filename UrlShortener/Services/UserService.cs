9577ad84db8a6a86353d1454ebf9ccca3d5e4189
Orangekun <amazingritro66@gmail.com>
2024-08-05 00:46:49 +0300

    added UserService implementation

---
+++ UrlShortener/Services/UserService.cs
@@ -0,0 +1,35 @@
+
+
+using UrlShortener.Models;
+using UrlShortener.Repositories;
+
+namespace UrlShortener.Services
+{
+    public class UserService(IUserRepository repository) : IUserService
+    {
+        public async Task CreateUserAsync(User user)
+        {
+            await repository.AddUserAsync(user);
+        }
+
+        public async Task DeleteUserAsync(int id)
+        {
+            await repository.DeleteUserAsync(id);
+        }
+
+        public async Task<User> GetUserByIdAsync(int id)
+        {
+            return await repository.GetUserByIdAsync(id);
+        }
+
+        public async Task<User> GetUserByUserNameAsync(string userName)
+        {
+            return await repository.GetUserByUserNameAsync(userName);
+        }
+
+        public async Task UpdateUserAsync(User user)
+        {
+            await repository.UpdateUserAsync(user);
+        }
+    }
+}
