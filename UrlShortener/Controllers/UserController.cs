d1730431b5bdf1059a788949d7c3d84f7d4c5abe
Orangekun <amazingritro66@gmail.com>
2024-08-13 23:21:49 +0300

    added log statements

--- UrlShortener/Controllers/UserController.cs
+++ UrlShortener/Controllers/UserController.cs
@@ -8,7 +8,7 @@ namespace UrlShortener.Controllers
 {
     [ApiController]
     [Route("api/user")]
-    public class UserController(IUserService userService, ITokenService tokenService) : ControllerBase
+    public class UserController(IUserService userService, ITokenService tokenService, ILogger<UserController> logger) : ControllerBase
     {
         [HttpGet]
         [Authorize]
@@ -20,14 +20,17 @@ namespace UrlShortener.Controllers
                 var userClaim = User.FindFirst(ClaimTypes.NameIdentifier);
                 int id = int.Parse(userClaim?.Value ?? throw new ArgumentException("User not found"));
                 User user = await userService.GetUserByIdAsync(id);
+                logger.LogInformation("User :{userId} has requested their profile", id);
                 return user == null ? NotFound("User not found") : Ok(user);
             }
             catch (ArgumentException ex)
             {
+                logger.LogError(ex, "An ArgumentException occurred while getting the user: {userId}", User.Identity.Name);
                 return BadRequest(ex.Message);
             }
             catch (Exception ex)
             {
+                logger.LogError(ex, "An exception occurred while getting the user: {userId}", User.Identity.Name);
                 return StatusCode(500, ex.ToString());
             }
 
@@ -39,15 +42,19 @@ namespace UrlShortener.Controllers
             try
             {
                 await userService.CreateUserAsync(user);
+                logger.LogInformation("User :{userId} has registered", user.Id);
                 return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
             }
             catch (ArgumentException e)
             {
+                logger.LogError(e, "An ArgumentException occurred while registering the user: {userId}", user.Id);
+                logger.LogDebug(e, "An ArgumentException occurred while registering a user {error}", e.Message);
                 return BadRequest(e.Message);
             }
-            catch (Exception)
+            catch (Exception e)
             {
-
+                logger.LogError("An exception occurred while registering the user: {userId}", user.Id);
+                logger.LogDebug(e, "An exception occurred while registering a user {error}", e.Message);
                 return StatusCode(500, "Internal server error");
             }
         }
@@ -70,19 +77,22 @@ namespace UrlShortener.Controllers
                     SameSite = SameSiteMode.Strict,
                     Secure = true
                 });
-
+                logger.LogInformation("User :{userId} has refreshed their token", userId);
                 return Ok(new { AccessToken = accessToken });
             }
             catch (UnauthorizedAccessException ex)
             {
+                logger.LogError(ex, "An UnauthorizedAccessException occurred while refreshing the token: {userId}", User.Identity?.Name);
                 return Unauthorized(ex.Message);
             }
             catch (ArgumentException ex)
             {
+                logger.LogError(ex, "An ArgumentException occurred while refreshing the token: {userId}", User.Identity?.Name);
                 return BadRequest(ex.Message);
             }
             catch (Exception)
             {
+                logger.LogError("An exception occurred while refreshing the token: {userId}", User.Identity?.Name);
                 return StatusCode(500, "Internal server error");
             }
         }
